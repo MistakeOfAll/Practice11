f38d1dba0e9fc6722b63f094a1849b5f3d4a7e75
Your Name <you@example.com>
2024-02-07 01:53:24 +0300

    commit

--- PraktikaFurniture/obj/Debug/net7.0-windows/MainPage.g.i.cs
+++ PraktikaFurniture/obj/Debug/net7.0-windows/MainPage.g.i.cs
@@ -109,7 +109,7 @@ namespace PraktikaFurniture {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/PraktikaFurniture;V1.0.0.0;component/mainpage.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/PraktikaFurniture;V1.0.0.1;component/mainpage.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainPage.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
